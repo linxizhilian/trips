83ede32044378167a30325125c021d2a40a816cd
kanglei <kanglei@xmeise.com>
2017-08-24 09:34:34 +0800

    '测试一下忽略文件'

--- CI/appadmin/config/database.php
+++ CI/appadmin/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost22',
 	'username' => '',
 	'password' => '',
 	'database' => '',
