0a7cf725a5d25770c271c4a28beff6a27596f9d4
herbomiy <feng_yanxian@163.com>
2017-11-19 21:35:31 +0800

    首页 - 导航 - 适配

--- CI/application/views/footer.php
+++ CI/application/views/footer.php
@@ -1,12 +1,43 @@
 <style>
-	.footer	{margin: auto;}
-	.footer { width:100%; height:120px; background:#2c3a46; }
-	.footer { bottom: 0px; }
-	.footer .l img { margin-top:30px; }
-	.footer .r { margin-left:75px; }
-	.footer .r .link { height:32px; line-height:32px; margin-top:28px; }
-	.footer .r .link a { font-size:14px; color:#fff; margin-right:43px; }
-	.footer .r .p { line-height:32px; font-size:14px; color:#999; }
+	.footer {
+		margin: auto;
+	}
+
+	.footer {
+		width: 100%;
+		height: 120px;
+		background: #2c3a46;
+	}
+
+	.footer {
+		bottom: 0px;
+	}
+
+	.footer .l img {
+		margin-top: 30px;
+	}
+
+	.footer .r {
+		margin-left: 75px;
+	}
+
+	.footer .r .link {
+		height: 32px;
+		line-height: 32px;
+		margin-top: 28px;
+	}
+
+	.footer .r .link a {
+		font-size: 14px;
+		color: #fff;
+		margin-right: 43px;
+	}
+
+	.footer .r .p {
+		line-height: 32px;
+		font-size: 14px;
+		color: #999;
+	}
 </style>
 <div class="sub-footer" style="">
 	<div class="container">
@@ -29,12 +60,31 @@
 	</div>
 </div>
 <!--link-js-->
-<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js"
+				integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+				crossorigin="anonymous"></script>
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				crossorigin="anonymous"></script>
 <script src="http://s.lelezone.com/baike/js/jquery-1.7.2.min.js" type="text/javascript"></script>
 <script src="/plugins/assets/js/js.js?v=1213" type="text/javascript"></script>
 
-<script >
+<script>
+
+	$(document).ready(function () {
+		// 初始化
+		// list 打开
+		console.log(' ready ')
+		$('.menu-group-btn').on('click', function () {
+			$('.menu-group-content').toggleClass('view-menu')
+
+		})
+		// list 关闭
+		$('.menu-content-close').on('click', function () {
+
+			$('.menu-group-content').toggleClass('view-menu')
+		})
+	});
 
 	//根据图片样式的宽高动态加载图片地址
 	function checkShow() {//检查元素是否在可视范围内
@@ -68,6 +118,8 @@
 			checkShow();
 		});
 	});
+
+
 </script>
 </body>
 </html>
--- CI/application/views/nav.php
+++ CI/application/views/nav.php
@@ -10,25 +10,13 @@
 <!--	</li>-->
 <!--</ul>-->
 <header id="homepage-header" class="header">
-	<div class="header-logo"><a class="header-link" href="/">
-			<div class="js-logo-container black"><i>
-					<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 835.6 98.4" class="js-log">
-						<style>.js-log .st0 {
-								fill: #363636
-							}
-							.js-log .st1 {
-								opacity: .53;
-								fill: #0ff
-							}
-
-							.js-log .st2 {
-								fill: #fff
-							}</style>
-						<path class="st0"
-									d="M16.8 69c1.1 9.2 6.1 14.6 15.2 14.6 10.9 0 16.2-6.2 16.2-19.7V1.4h16.2v62.5c0 22.7-12.4 34.5-32 34.5C11.8 98.4 1.4 85.9 0 69h16.8zM160.3 1.4v14.3h-46.5v25.2H156V55h-42.1v27.3h46.5v14.5H98V1.4h62.3zm24.1 0H264v14.3h-31.7v81.1h-16.2V15.7h-31.7V1.4zm108.9 72.1c3.9 11.5 14 17.6 27 17.6 13.7 0 23.8-7 23.8-19.1 0-8.4-4.3-14.3-16.5-17.7l-18.2-5.1c-13.4-3.7-21.9-9.8-21.9-23.3C287.6 9.6 301.3 0 317.7 0c18.6 0 29.8 9.8 33.6 23.9h-7.9c-3.6-10.1-11.8-16.8-25.6-16.8-12.4 0-22.5 6.5-22.5 18.2 0 10.1 6.5 13.7 18.2 16.9l16 4.7C345.2 51.3 352 59 352 71.6c0 16.8-13.7 26.6-31.5 26.6-17.6 0-31.5-8.9-35.3-24.7h8.1zM442.2 1.4v7.3h-51.4V44h46.6v7.3h-46.6v38.2h51.4v7.3H383V1.4h59.2zm25.8 0h77.7v7.3h-35v88.1h-7.9V8.7H468V1.4zm99.7 0h77.7v7.3h-35v88.1h-7.9V8.7h-34.8V1.4zm166.4 0v7.3h-51.4V44h46.6v7.3h-46.6v38.2h51.4v7.3h-59.2V1.4h59.2zm41.2 53.3v42.1h-7.9V1.4h33.7c18.6 0 30 9.3 30 26.7 0 15.5-9.2 24.4-24.2 26.3l28.7 42.4h-9.2l-28.3-42.1h-22.8zm0-7h25.5c14.6 0 22.5-7.3 22.5-19.6 0-12.6-7.9-19.6-22.5-19.6h-25.5v39.2z"></path>
-					</svg>
-				</i></div>
-		</a></div>
+	<div class="header-logo">
+		<a class="header-link" href="/">
+			<div class="js-logo-container black">
+				<img src="/plugins/assets/images/logo.png" alt="...">
+			</div>
+		</a>
+	</div>
 	<div class="header-center">
 		<div class="header-linkbin">
 			<div class="new-links">
@@ -45,4 +33,30 @@
 		<p>Wechat: 86-18551151099</p>
 		<p>Email: book@catherinelutours.com</p>
 	</div>
+
+	<div class="menu-group-btn menu-group">
+		<i class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></i>
+	</div>
+
+
+
 </header>
+<div class="menu-group-content">
+	<div class="menu-group-content-header"><i class="glyphicon glyphicon-remove menu-content-close cursor-pointer" aria-hidden="true"></i></div>
+	<ul class="menu-slider-list">
+		<li class="link-container cursor-pointer"><a href="/Index/home">首页</a></li>
+		<li class="link-container cursor-pointer"><a href="/Category/index">分类页</a></li>
+		<li class="link-container cursor-pointer"><a href="/Articles/index">文章页</a></li>
+		<li class="link-container cursor-pointer"><a href="/Articles/index">我的信息</a></li>
+		<li class="link-container cursor-pointer"><a href="/Articles/index">宣&emsp;&emsp;言</a></li>
+		<li class="link-container cursor-pointer"><a href="/Articles/index">关于我们</a></li>
+		<div class="menu-contact-us">
+			<p>Wechat: 86-18551151099</p>
+			<p>Email: book@catherinelutours.com</p>
+		</div>
+	</ul>
+</div>
+
+
+
+
