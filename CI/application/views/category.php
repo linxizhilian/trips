a5c4cc34ff74e5e3e304d021d950769767b29b8c
herbomiy <feng_yanxian@163.com>
2017-11-27 22:47:07 +0800

    首页 - 导航 - 适配

--- CI/application/views/category.php
+++ CI/application/views/category.php
@@ -1,168 +1,191 @@
-
 <?php
 $this->load->view('header');
 ?>
-<body id="index" class="main-container">
+<body id="index" class="main-container main-category">
 <div class="row masonry-container  container-fluid">
-    <div class="row-fluid">
-        <div class="span12">
-					<?php
-					$this->load->view('nav');
-					?>
+	<div class="row-fluid">
+		<div class="span12">
+			<?php
+			$this->load->view('nav');
+			?>
 
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="/plugins/assets/images/1.jpeg" alt="...">
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="/plugins/assets/images/3.jpeg" alt="...">
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="/plugins/assets/images/7.jpeg" alt="...">
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                </div>
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img src="/plugins/assets/images/1.jpeg" alt="...">
+						<div class="carousel-caption">
+						</div>
+					</div>
+					<div class="item">
+						<img src="/plugins/assets/images/3.jpeg" alt="...">
+						<div class="carousel-caption">
+						</div>
+					</div>
+					<div class="item">
+						<img src="/plugins/assets/images/7.jpeg" alt="...">
+						<div class="carousel-caption">
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
 
-            <!-- 面包屑 -->
-            <ul class="breadcrumb">
-							<li><a href="/Index/home">主页</a> <span class="divider"></span></li>
-							<li><a href="/Category/index">类目</a> <span class="divider"></span></li>
-            </ul>
+			<!-- 面包屑 -->
+			<ul class="breadcrumb">
+				<li><a href="/Index/home">主页</a> <span class="divider"></span></li>
+				<li><a href="/Category/index">类目</a> <span class="divider"></span></li>
+			</ul>
 
-            <!-- 文章类表 -->
-            <div class="col-md-12 col-sm-12" style="padding: 0;">
-                <ul class="thumbnails" style="padding: 0;">
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    冯诺尔曼结构
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    哈佛结构
-                                </p>
+			<!-- 分类列表 -->
+			<div class="media-container">
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://p1.so.qhimgs1.com/t0166e87962ad045a41.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li>Tour code: CL-D-04B</li>
+							<li>Available: Daily</li>
+							<li>Duration: Beijing Four Full Days</li>
+							<li>Starts/ends: On your request</li>
+							<li>Attractions: The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://picture.youth.cn/qtdb/201703/W020170304317792550271.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li>Tour code: CL-D-04B</li>
+							<li>Available: Daily</li>
+							<li>Duration: Beijing Four Full Days</li>
+							<li>Starts/ends: On your request</li>
+							<li>Attractions: The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://localhost/plugins/assets/images/1.jpeg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://p1.so.qhimgs1.com/t0166e87962ad045a41.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://p1.so.qhimgs1.com/t0166e87962ad045a41.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://p1.so.qhimgs1.com/t0166e87962ad045a41.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://localhost/plugins/assets/images/1.jpeg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+				<div class="tour-item clear">
+					<div class="tour-item-hd col-sm-12 col-md-5 col-lg-5">
+						<img src="http://p1.so.qhimgs1.com/t0166e87962ad045a41.jpg" alt="景点图">
+					</div>
+					<div class="tour-item-bd col-sm-12 col-md-7 col-lg-7">
+						<h3>Beijing Private 1-Day Tour (Forbidden City＋Great Wall)</h3>
+						<ul class="list-style-dot">
+							<li><strong>Tour code:</strong> CL-D-04B</li>
+							<li><strong>Available:</strong> Daily</li>
+							<li><strong>Duration:</strong> Beijing Four Full Days</li>
+							<li><strong>Starts/ends:</strong> On your request</li>
+							<li><strong>Attractions:</strong> The Great Wall at Mutianyu, Tian’anmen Square, the Forbidden City, Temple of Heaven, Olympic Stadiums, Summer Palace; Lama Temple, Jingshan Park, Beihai Park, Confucius Temple, Beijing Zoo (Panda House), National Museum, Acrobatic show or Kongfu show</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
-
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    改进型哈佛结构
-                                </p>
-
-
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    改进型哈佛结构
-                                </p>
-
-
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    改进型哈佛结构
-                                </p>
-
-
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    改进型哈佛结构
-                                </p>
-
-
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4" style="margin-bottom: 15px;">
-                        <div class="thumbnail">
-                            <img alt="300x200" src="/plugins/assets/images/2.jpeg" />
-                            <div class="caption">
-                                <p>
-                                    改进型哈佛结构
-                                </p>
-
-
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- 分页 -->
-            <nav aria-label="text-center Page navigation">
-                <ul class="pagination">
-                    <li>
-                        <a href="#" aria-label="Previous">
-                            <span aria-hidden="true">上一页</span>
-                        </a>
-                    </li>
-                    <li><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                    <li>
-                        <a href="#" aria-label="Next">
-                            <span aria-hidden="true">下一页</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
-
 
 <?php
 $this->load->view('footer');
 ?>
+
+<script>
+	$(document).ready(function () {
+		console.log(' -- 分组初始化 -- ')
+
+
+	});
+</script>
+
+
